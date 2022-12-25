f6c3664f239ae5b8ca25298d19ffd0bbbfea3af8
033_Alex-Pandian-M <alexpandian11205@gmail.com>
2022-12-25 18:52:55 +0530

    Create Arithmetic_calculations.py

---
+++ Arithmetic_calculations.py
@@ -0,0 +1,10 @@
+# Arithmetic calculations
+a=int(input('Enter number : '))
+b=int(input('Enter number : '))
+print(f'a+b = {a+b}')         # addition
+print(f'a-b = {a-b}')         # subtraction
+print(f'a*b = {a*b}')         # multiplication
+print(f'a/b = {a/b}')         # division
+print(f'a//b = {a//b}')       # floor division
+print(f'a**b = {a**b}')       # exponent
+print(f'a%b = {a%b}')         # integer division
